b574df9b0fcd5ee0975d734c007fa7137f3189b6
anishsharma21 <anisharma2103@gmail.com>
2024-08-10 17:48:14 +1000

    Nested class implementation

--- dotnet-xunit-1/src/Models/Pets/Dog.cs
+++ dotnet-xunit-1/src/Models/Pets/Dog.cs
@@ -2,6 +2,15 @@ namespace Models.Pets
 {
   public class Dog : IPet
   {
+    public class Tail
+    {
+      public int TailLength { get; set; }
+
+      public void PrintTailLength()
+      {
+        Console.WriteLine(TailLength);
+      }
+    }
     public string TalkToOwner() => "Woof!";
   }
 }
