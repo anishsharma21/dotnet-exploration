0e9dc0f6350144c32a36224286fb36ae4b06914e
anishsharma21 <anisharma2103@gmail.com>
2024-08-10 18:01:42 +1000

    Comparing the differences between abstract classes and interfaces
    - Comparing the use cases, its pretty clear that abstract classes allow for some already defined functionality that can be universally inherited if needed, while interfaces are just for the method signatures

--- dotnet-xunit-1/src/Models/Pets/Cat.cs
+++ dotnet-xunit-1/src/Models/Pets/Cat.cs
@@ -1,7 +1,12 @@
 namespace Models.Pets
 {
-  public class Cat : IPet
+  public class Cat : Pet
   {
-    public string TalkToOwner() => "Meow!";
+    public override string TalkToOwner() => "Meow!";
+
+    public override void Eating()
+    {
+      Console.WriteLine("munching");
+    }
   }
 }
--- dotnet-xunit-1/src/Models/Pets/Pet.cs
+++ dotnet-xunit-1/src/Models/Pets/Pet.cs
@@ -3,5 +3,10 @@ namespace Models.Pets
   public abstract class Pet : IPet
   {
     public abstract string TalkToOwner();
+
+    public virtual void Eating()
+    {
+      Console.WriteLine("Nom nom");
+    }
   }
 }
