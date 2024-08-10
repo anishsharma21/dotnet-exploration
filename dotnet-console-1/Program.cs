b589661fff2bf58c96ccf674f6312c7891959b25
anishsharma21 <anisharma2103@gmail.com>
2024-08-10 15:09:29 +1000

    Initial commit - packaging all dotnet app learning projects into one repository to make tracking easier and reduce clutter

---
+++ dotnet-console-1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace csharp_console_v2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(StartsWithUpper("Hello"));
+        Console.WriteLine(StartsWithUpper("hello"));
+    }
+
+    static bool StartsWithUpper(string str)
+    {
+        if (string.IsNullOrEmpty(str)) return false;
+        char ch = str[0];
+        return char.IsUpper(ch);
+    }
+}
