becaed982647f5b867f6dd574df42af1aa379b77
anishsharma21 <anisharma2103@gmail.com>
2024-08-11 15:08:45 +1000

    Using the GC class!

--- dotnet-xunit-1/src/Models/Program.cs
+++ dotnet-xunit-1/src/Models/Program.cs
@@ -11,13 +11,16 @@ namespace Models
         Field = "field init"
       };
       Console.WriteLine(simpleGeneric.Field);
+      Console.WriteLine(GC.GetTotalMemory(false));
 
       SimpleGeneric<int> simpleGeneric2 = new();
       Console.WriteLine(simpleGeneric2.GetType());
+      Console.WriteLine(GC.GetTotalMemory(false));
 
       LinkedList<string> strings = new();
       strings.AddFirst("First");
       List<string> newList = [.. strings];
+      Console.WriteLine(GC.GetTotalMemory(true));
     }
 
   }
