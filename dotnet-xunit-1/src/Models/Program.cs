8b5d2ea97a69f21ddb6732466858a712e890f513
anishsharma21 <anisharma2103@gmail.com>
2024-08-11 14:56:46 +1000

    Data structures in C#
    ArrayList, List, Dictionary, Hashtable - differences are in performance, dynamic ability to resize, and whether require the types to be specified (if so, better performance)

--- dotnet-xunit-1/src/Models/Program.cs
+++ dotnet-xunit-1/src/Models/Program.cs
@@ -1,16 +1,26 @@
-﻿namespace Models
+﻿using System.Collections;
+
+namespace Models
 {
   public class Program
   {
     public static void Main(string[] args)
     {
-      int[] baseValues = [2, 8, 10, 16];
-      short value = short.MaxValue;
-      foreach (int baseValue in baseValues)
+      Console.ForegroundColor = ConsoleColor.Blue;
+      ArrayList arrayList = [1, 2, 3, 4];
+      List<int> ints = [1, 2, 3, 4];
+      Dictionary<int, string> keyValuePairs = new()
       {
-        string s = Convert.ToString(value, baseValue);
-        short value2 = Convert.ToInt16(s, baseValue);
-        Console.WriteLine($"{value} --> {s}\n{baseValue} --> {value2}");
+          { 1, "one" },
+          { 2, "two" },
+          { 3, "three" }
+      };
+      Hashtable hashtable = [];
+      hashtable.Add("hi there", 1);
+
+      foreach (KeyValuePair<int, string> kvp in keyValuePairs)
+      {
+        Console.WriteLine($"Key: {kvp.Key}, Value: {kvp.Value}");
       }
     }
 
