d2bb48de2d35316531bab86337f4ce0ac47e1c61
anishsharma21 <anisharma2103@gmail.com>
2024-08-12 20:33:35 +1000

    Model creation for ASP.NET Web API
    - Created the Stock model

--- dotnet-console-1/Program.cs
+++ dotnet-console-1/Program.cs
@@ -1,25 +1,31 @@
 ﻿namespace csharp_console_v2;
 
+// ReSharper disable once ClassNeverInstantiated.Global
 class Program
 {
     static async Task Main(string[] args)
     {
+        foreach (var arg in args)
+        {
+            Console.WriteLine(arg);
+        }
+        
         Console.WriteLine(StartsWithUpper("Hello"));
         Console.WriteLine(StartsWithUpper("hello"));
         double returnedNum = await ReturnNumber(5);
         Console.WriteLine(returnedNum);
     }
 
-    static bool StartsWithUpper(string str)
+    private static bool StartsWithUpper(string str)
     {
         if (string.IsNullOrEmpty(str)) return false;
         char ch = str[0];
         return char.IsUpper(ch);
     }
 
-    static async Task<double> ReturnNumber(int numerator)
+    private static async Task<double> ReturnNumber(int numerator)
     {
-        int divisor = 4;
+        const int divisor = 4;
         await Task.Delay(2000);
         return (double)numerator / divisor;
     }
