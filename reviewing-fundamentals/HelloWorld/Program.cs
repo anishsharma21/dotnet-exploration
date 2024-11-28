19195c2cc175e9332d889711f3dc2ccca76898fa
anishsharma21 <anisharma2103@gmail.com>
2024-11-28 21:38:00 +1100

    Covering more fundamental control flow and C# concepts, just review, getting back into it.

--- reviewing-fundamentals/HelloWorld/Program.cs
+++ reviewing-fundamentals/HelloWorld/Program.cs
@@ -1,10 +1,33 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Runtime.InteropServices;
+
+Console.WriteLine("Hello, World!");
 
 const int age = 25;
 
 Console.WriteLine(age);
 
-for (var i = 0; i < 100; i++)
+int[] numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+Console.WriteLine(numbers[1]);
+
+List<string> fruits = ["apple", "orange", "grape"];
+
+foreach (var fruit in fruits)
 {
-    Console.WriteLine(i);
+    Console.WriteLine($"Fruit: {fruit}");
+}
+
+GreetUser("John", 4);
+GetInput();
+return;
+
+void GreetUser(string name, int age)
+{
+    Console.WriteLine($"{name} is {age} years old!");
+}
+
+void GetInput()
+{
+    Console.Write("Number: ");
+    var line = Console.ReadLine();
+    Console.WriteLine($"Users value: {line}");
 }
